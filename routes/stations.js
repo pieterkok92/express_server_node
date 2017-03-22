5c38042cfdc37becc963a4d9bd731bee3e310371
Unknown <pieter.kok@nampower.com.na>
2017-03-22 11:59:50 +0200

    33

--- routes/stations.js
+++ routes/stations.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
-var http = require('http');
+
 // Connection URL
 var url = 'mongodb://admin:1234@ds129720.mlab.com:29720/heroku_n1kxz9pj';
 
@@ -9,13 +9,21 @@ var url = 'mongodb://admin:1234@ds129720.mlab.com:29720/heroku_n1kxz9pj';
 /* GET home page. */
 router.get('/', function(req, res, next) {
  // Use connect method to connect to the server
-    
+    MongoClient.connect(url, function(err, db) {
+        if(err){
+            return res.json( {success: false, message: err });
+        }
 
-    var app = http.createServer(function(req,res){
-    res.setHeader('Content-Type', './substations.geojson');
-    res.send(JSON.stringify({}, null, 3));
-    response.write(JSON.stringify('./substations.geojson'));
-});
+
+        db.collection('stations').find({}).toArray((err, stations)=>{
+            if(err){
+                return res.json( {success: false, message: err });
+            }
+
+            return res.json({success: true, stations: stations});
+        });
+
+    });
       
   
 });
