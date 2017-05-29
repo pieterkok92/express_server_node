a3a63f37d23dea7af8c74fb12ef73a37f188f297
Kok <pieter.kok@nampower.com.na>
2017-05-29 09:48:48 +0100

    2

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.get('/upload', function(req, res){
 
 app.get('/upload2', function(req, res){
 
-  let powerlines = require('./powerlines.json');
+  let powerlines = require('./P.geojson');
 
   let powerlinesArray = powerlines['features'];
 
