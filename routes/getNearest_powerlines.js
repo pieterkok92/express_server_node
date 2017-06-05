1f61b35d912dea3733c970a11aa9e78b19f3267f
Kok <pieter.kok@nampower.com.na>
2017-06-05 08:52:11 +0100

    2

--- routes/getNearest_powerlines.js
+++ routes/getNearest_powerlines.js
@@ -20,7 +20,7 @@ router.get('/', function(req, res, next) {
         if(err){
             return res.json( {success: false, message: err });
         }
-          db.collection('powerlines').find({}).toArray((err, stations)=>{
+          db.collection('powerlines').find({}).limit(5000).toArray((err, stations)=>{
             if(err){
                 return res.json( {success: false, message: err });
             }
