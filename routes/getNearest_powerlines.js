bc31fe4ad210927bff68749467a9cf95d947371b
Kok <pieter.kok@nampower.com.na>
2017-06-05 11:19:17 +0100

    23

--- routes/getNearest_powerlines.js
+++ routes/getNearest_powerlines.js
@@ -41,14 +41,14 @@ router.get('/', function(req, res) {
             });
         });
 
-        fs.writeFile('test.txt', path,  function(err) {
+        fs.writeFile('./test.txt', path,  function(err) {
         if (err) {
             return console.error(err);
         }
         
         console.log("Data written successfully!");
         console.log("Let's read newly written data");
-        fs.readFile('input.txt', function (err, data) {
+        fs.readFile('./test.txt', function (err, data) {
             if (err) {
                 return console.error(err);
             }
