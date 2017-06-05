f06544b925b87dea316f51174a3c36ef770a915f
Kok <pieter.kok@nampower.com.na>
2017-06-05 16:07:28 +0100

    1312412

--- routes/getNearest_powerlines.js
+++ routes/getNearest_powerlines.js
@@ -1,20 +1,17 @@
+var Dropbox = require('../../src/dropbox');
+var fs = require('fs');
+var path = require('path');
+var prompt = require('prompt');
 var express = require('express');
 var mongoose = require('mongoose');
 var MongoClient = require('mongodb').MongoClient;
-var router = express.Router();
 var url = 'mongodb://admin:1234@ds129720.mlab.com:29720/heroku_n1kxz9pj';
-var geolib = require('geolib');
 var fs = require('fs');
 mongoose.connect(url);
-var node_dropbox = require('node-dropbox');
-
-
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
-
-//geolib.getDistance(object start, object end)
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -23,7 +20,7 @@ router.get('/', function(req, res) {
         if(err){
             return res.json( {success: false, message: err });
         }
-		var data;
+		    var data;
         db.collection('powerlines', function(err, collection) 
         {
             collection.find({}).toArray(function(err, results) 
@@ -34,6 +31,31 @@ router.get('/', function(req, res) {
                 
             });
         });
+    });
+
+    prompt.start();
+
+    prompt.get({
+      properties: {
+        accessToken: {
+          description: 'Please enter an API V2 access token'
+        }
+      }
+    }, function (error, result) {
+      var dbx = new Dropbox({ accessToken: result.accessToken });
+
+      
+
+        // This uploads basic.js to the root of your dropbox
+        dbx.filesUpload({ path: '/Apps/nodejs/test.json', data: data })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (err) {
+            console.log(err);
+          });
+      });
+    });
 
 		node_dropbox.Authenticate('os6d36r922c1xbv', 'w5t5w06m409xrl5', function(err, url){
 			// redirect user to the url.
